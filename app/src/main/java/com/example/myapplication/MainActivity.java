5fc24e5a8ecf1c09a2fa01c9e8b545f5456224cf
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-04 04:30:34 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            //134
+            //1343333333333333333333333
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragment_Main()).commit();
+        System.out.println("trinh duc hùng");
     }
 }
