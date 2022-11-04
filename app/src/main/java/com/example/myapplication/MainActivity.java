35702f7f98cfc64783d0aa80751572912df61d76
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-04 10:07:09 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
         //1343333
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragment_Main()).commit();
         System.out.println("Hug sieu cap vippro");
-        for (int i = 0; i < i; i++) {
-            System.out.println(i+1);
-            System.out.println("trinh duc 13123123gung");
-        }
+     
     }
 }
