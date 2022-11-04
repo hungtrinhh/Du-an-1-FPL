b4469b2b6b9adab5642e51e7bf94d224f5c3f15e
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-04 11:49:19 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.printf("Hun123123");
+        System.out.printf("3123123");
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragment_Main()).commit();
     }
 }
