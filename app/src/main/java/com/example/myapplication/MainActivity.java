dd3bf3ec60ce6726fa7c3aedce8bbaceaab80b7b
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-04 14:12:25 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.myapplication.fragment;
 import com.example.myapplication.fragment.fragment_Main;
 
 public class MainActivity extends AppCompatActivity {
