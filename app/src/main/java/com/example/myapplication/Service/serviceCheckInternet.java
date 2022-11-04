1a8f9a355dc5a45a1b8178a76212351714d8c5f0
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-04 22:15:53 +0700

    Create serviceCheckInternet.java

---
+++ app/src/main/java/com/example/myapplication/Service/serviceCheckInternet.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.Service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class serviceCheckInternet extends IntentService {
+
+
+
+    public serviceCheckInternet() {
+        super("serviceCheckInternet");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
