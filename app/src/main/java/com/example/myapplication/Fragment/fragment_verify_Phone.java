bd67516232f553d1a1f463b57a58519f63b36274
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-06 06:58:18 +0700

    Update fragment_verify_Phone.java

--- app/src/main/java/com/example/myapplication/Fragment/fragment_verify_Phone.java
+++ app/src/main/java/com/example/myapplication/Fragment/fragment_verify_Phone.java
@@ -33,13 +33,13 @@ public class fragment_verify_Phone extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_verify_phone_numbers, container, false);
     }
 }
