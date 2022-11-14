b4acf24d83856e1781661c9345a9b884d87f4bef
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-14 15:11:53 +0700

    Update FbDao.java

--- app/src/main/java/com/example/myapplication/Firebase/FbDao.java
+++ app/src/main/java/com/example/myapplication/Firebase/FbDao.java
@@ -1,19 +1,15 @@
 package com.example.myapplication.Firebase;
 
-import android.app.Activity;
+
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.Model.Game;
 import com.example.myapplication.Model.User;
 import com.example.myapplication.Model.Voucher;
-import com.google.firebase.FirebaseException;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +21,26 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class FbDao {
+//    public void Test(String string) {
+//        Log.d(TAG, "string is: " + string);
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue(string);
+//
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//    }
 
 
     public FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -104,26 +120,6 @@ public class FbDao {
         });
     }
 
-    public void Test(String string) {
-        Log.d(TAG, "string is: " + string);
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue(string);
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-    }
 
     public void AddUser(User user) {
         DatabaseReference myRef = database.getReference("Users");
